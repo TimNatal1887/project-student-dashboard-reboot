a68c18e9bcdeb7b02a21c0121949a667845e5a17
Haiyah Perez <haiyahperez@pursuit.org>
2024-01-18 12:12:34 -0500

    student details started

--- src/StudentDetails.jsx
+++ src/StudentDetails.jsx
@@ -1,7 +1,63 @@
 import { useParams } from "react-router-dom"
-export default function StudentDetails(){
+import { formatBirthDate, onTrackChecker } from "./Student"
+
+export default function StudentDetails({students}){
     const {id} = useParams()
+    console.log(id)
+    console.log(students)
+    
+    const studentInfo = students.find((student) => id === student.id)
+    console.log(studentInfo)
+    // const birthday = formatBirthDate(studentInfo.dob)
+    const fullName = `${studentInfo.names.preferredName} ${studentInfo.names.middleName} ${studentInfo.names.surname}`
+    const {current, goal} = studentInfo.codewars 
+    const {scores} = studentInfo.cohort
+    const {certifications} = studentInfo
     return (
-        <h1>hi im paul</h1>
+        <div>
+            <div>
+                <img src= {studentInfo.profilePhoto} alt="Student Thumbnail"></img>
+                <p>{fullName}</p>
+                <p>{studentInfo.username}</p>
+                <p>{birthday}</p>
+                { onTrackChecker(studentInfo) && <p className="onTrack">On track to Graduate </p> }
+            </div>
+            <table>
+      <thead>
+        <tr>
+          <th>CodeWars</th>
+          <th>Scores</th>
+          <th>Certifications</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Current Total: {current.total}</td>
+          <td>Cell 1,2</td>
+          <td>Cell 1,3</td>
+        </tr>
+        <tr>
+          <td>Cell 2,1</td>
+          <td>Cell 2,2</td>
+          <td>Cell 2,3</td>
+        </tr>
+        <tr>
+          <td>Cell 3,1</td>
+          <td>Cell 3,2</td>
+          <td>Cell 3,3</td>
+        </tr>
+        <tr>
+          <td>Cell 4,1</td>
+          <td>Cell 4,2</td>
+          <td>Cell 4,3</td>
+        </tr>
+      </tbody>
+    </table>
+            <form>
+            </form>
+
+        </div>
+        
+
     )
 }

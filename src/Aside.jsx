713906d4a25c99e5806805c276986f624f0bb15a
Timothy Natal <timothynatal@pursuit.org>
2024-01-18 08:18:27 -0500

    add cohort filtering

--- src/Aside.jsx
+++ src/Aside.jsx
@@ -1,55 +1,57 @@
 import React from "react";
 import "./Aside.css";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 const Aside = () => {
+    
   return (
     <aside>
       <h3> Choose A Class By Start Date </h3>
       <button className="sort-button">Sort Ascending By Year</button>
       <ul>
         <li>
-          <NavLink exact to="/" className="cohort-NavLink" activeClassName="activeLink">
+          <NavLink to="/" className="cohort-NavLink" activeclassname="activeLink">
             All Students
           </NavLink>
         </li>
         <li>
-          <NavLink to="/Winter2026" className="cohort-NavLink" activeClassName="activeLink">
+          <NavLink to="/Winter2026" className="cohort-NavLink" activeclassname="activeLink">
             Winter 2026
           </NavLink>
         </li>
         <li>
-          <NavLink to="/Spring2026" className="cohort-NavLink" activeClassName="activeLink">
+          <NavLink to="/Spring2026" className="cohort-NavLink" activeclassname="activeLink">
             Spring 2026
           </NavLink>
         </li>
         <li>
-          <NavLink to="/Summer2026" className="cohort-NavLink" activeClassName="activeLink">
+          <NavLink to="/Summer2026" className="cohort-NavLink" activeclassname="activeLink">
             Summer 2026               
           </NavLink>
         </li>
         <li>
-          <NavLink to="/Fall2026" className="cohort-NavLink" activeClassName="activeLink">
+          <NavLink to="/Fall2026" className="cohort-NavLink" activeclassname="activeLink">
             Fall 2026
           </NavLink>
         </li>
         <li>
-          <NavLink to="/Winter2025" className="cohort-NavLink" activeClassName="activeLink">
+          <NavLink to="/Winter2025" className="cohort-NavLink" activeclassname="activeLink">
             Winter 2025
           </NavLink>
         </li>
         <li>
-          <NavLink to="/Spring2025" className="cohort-NavLink" activeClassName="activeLink">
+          <NavLink to="/Spring2025" className="cohort-NavLink" activeclassname="activeLink">
             Spring 2025
           </NavLink>
         </li>
         <li>
-          <NavLink to="/Summer2025" className="cohort-NavLink" activeClassName="activeLink">
+          <NavLink to="/Summer2025" className="cohort-NavLink" activeclassname="activeLink">
             Summer 2025
           </NavLink>
         </li>
         <li>
-          <NavLink to="/Winter2025" className="cohort-NavLink" activeClassName="activeLink">
+          <NavLink to="/Winter2025" className="cohort-NavLink" activeclassname="activeLink">
             Winter 2025
           </NavLink>
         </li>
--- src/StudentList.jsx
+++ src/StudentList.jsx
@@ -1,9 +1,15 @@
 import Student from "./Student.jsx"
+import { useParams } from "react-router-dom"
 export default function StudentList({students}){
-
+    const {cohort} = useParams()
+    let filteredStudents = students
+    if(cohort){
+        filteredStudents = students.filter(student=>student.cohort.cohortCode === cohort)
+    }
+    console.log(filteredStudents)
     return (
         <ul className="student-list">
-            {students.map(student=><Student student={student} key={student.id}/>)}
+            {filteredStudents.map(student=><Student student={student} key={student.id}/>)}
         </ul>
     )
 }
